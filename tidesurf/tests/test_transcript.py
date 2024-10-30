256c8d46906b92b3aec42f0718c790925441d20e
Jan Schleicher <jan.schleicher@uni-tuebingen.de>
2024-10-30 10:11:50 +0100

    Set up test suite.

---
+++ tidesurf/tests/test_transcript.py
@@ -0,0 +1,240 @@
+import pytest
+
+from tidesurf.transcript import GenomicFeature, Exon, Transcript, TranscriptIndex
+
+TEST_GTF_FILE = "test_data/genes.gtf"
+
+
+def test_genomic_feature():
+    gen_feat_1 = GenomicFeature(
+        "",
+        "",
+        "",
+        "",
+        "chr1",
+        "+",
+        9_000_000,
+        9_001_000,
+    )
+    gen_feat_2 = GenomicFeature(
+        "",
+        "",
+        "",
+        "",
+        "chr1",
+        "+",
+        9_000_500,
+        9_001_000,
+    )
+    gen_feat_3 = GenomicFeature(
+        "",
+        "",
+        "",
+        "",
+        "chr1",
+        "+",
+        9_000_000,
+        9_001_500,
+    )
+    gen_feat_4 = GenomicFeature(
+        "",
+        "",
+        "",
+        "",
+        "chr2",
+        "+",
+        9_000_000,
+        9_001_500,
+    )
+    assert gen_feat_1 < gen_feat_2
+    assert gen_feat_1 < gen_feat_3
+    assert gen_feat_3 < gen_feat_2
+
+    assert gen_feat_2 > gen_feat_1
+    assert gen_feat_3 > gen_feat_1
+    assert gen_feat_2 > gen_feat_3
+
+    with pytest.raises(ValueError):
+        gen_feat_1 < gen_feat_4
+    with pytest.raises(ValueError):
+        gen_feat_4 > gen_feat_1
+
+    assert str(gen_feat_1).startswith(
+        "<GenomicFeature chr1:9,000,000-9,001,000 on '+' strand at 0x"
+    )
+
+    start, end = 8_999_900, 9_000_100
+    assert gen_feat_1.overlaps(
+        "chr1", "+", start, end
+    ), f"Genomic feature {gen_feat_1} should overlap region {start:,}-{end:,}."
+    assert not gen_feat_2.overlaps(
+        "chr1", "+", start, end
+    ), f"Genomic feature {gen_feat_2} should not overlap region {start:,}-{end:,}."
+
+
+def test_transcript():
+    exon_1 = Exon(
+        "ENSMUSG00000025911",
+        "Adhfe1",
+        "ENSMUST00000144177",
+        "Adhfe1-203",
+        "chr1",
+        "+",
+        9_547_948,
+        9_548_151,
+        "ENSMUSE00000754750",
+        1,
+    )
+    assert str(exon_1).startswith(
+        "<Exon ENSMUSE00000754750, No. 1 for transcript "
+        "ENSMUST00000144177 chr1:9,547,948-9,548,151 on '+' strand at 0x"
+    )
+    exon_2 = Exon(
+        "ENSMUSG00000025911",
+        "Adhfe1",
+        "ENSMUST00000144177",
+        "Adhfe1-203",
+        "chr1",
+        "+",
+        9_549_907,
+        9_549_944,
+        "ENSMUSE00001146417",
+        2,
+    )
+
+    transcript_1 = Transcript(
+        "ENSMUSG00000025911",
+        "Adhfe1",
+        "ENSMUST00000144177",
+        "Adhfe1-203",
+        "chr1",
+        "+",
+        9_547_948,
+        9_577_970,
+        [],
+    )
+    assert str(transcript_1).startswith(
+        "<Transcript ENSMUST00000144177 chr1:9,547,948-9,577,970 on '+' strand at 0x"
+    )
+    assert transcript_1.exons == [], "Exons should be empty list."
+    # Test adding exons
+    transcript_1.add_exon(exon_1)
+    transcript_1.add_exon(exon_2)
+    assert transcript_1.exons == [exon_1, exon_2], "Exons were not added correctly."
+    # Exon that is already present should not be added again
+    transcript_1.add_exon(exon_1)
+    assert transcript_1.exons == [
+        exon_1,
+        exon_2,
+    ], "Exon that was already present was not handled correctly."
+
+    transcript_2 = Transcript(
+        "ENSMUSG00000025911",
+        "Adhfe1",
+        "ENSMUST00000144177",
+        "Adhfe1-203",
+        "chr1",
+        "+",
+        9_547_948,
+        9_577_970,
+        [exon_1, exon_2],
+    )
+    assert transcript_1 == transcript_2, "Transcripts should be identical."
+
+
+def test_transcript_index():
+    transcript_idx = TranscriptIndex(gtf_file=TEST_GTF_FILE)
+
+    # Check that the index is constructed as expected
+    for (
+        chromosome,
+        strand,
+    ), regions in transcript_idx.transcripts_by_region.items():
+        assert regions == sorted(
+            regions, key=lambda x: x[0]
+        ), f"Regions for chromosome {chromosome} {strand} are not sorted"
+        for pos, transcripts in regions:
+            for transcript in transcripts:
+                assert (
+                    transcript.chromosome == chromosome
+                ), f"Chromosome mismatch: {transcript.chromosome} != {chromosome}"
+                assert (
+                    transcript.strand == strand
+                ), f"Strand mismatch: {transcript.strand} != {strand}"
+                assert (
+                    transcript.start <= pos <= transcript.end
+                ), f"Transcript {transcript.transcript_id} does not overlap region starting at {pos}"
+
+    # Test that no transcripts are returned when the chromosome/strand
+    # is not in the index
+    chromosome, strand, start, end = "chr22", "+", 1_234_567, 1_234_667
+    assert not transcript_idx.get_overlapping_transcripts(
+        chromosome=chromosome, strand=strand, start=start, end=end
+    )
+
+    # Test that no transcripts are returned when there is no overlap
+    chromosome, strand, start, end = "chr1", "+", 8_000_000, 8_000_150
+    assert not transcript_idx.get_overlapping_transcripts(
+        chromosome=chromosome, strand=strand, start=start, end=end
+    )
+    start, end = 9_908_450, 9_908_600
+    assert not transcript_idx.get_overlapping_transcripts(
+        chromosome=chromosome, strand=strand, start=start, end=end
+    )
+
+    # Test that the correct overlapping transcripts are returned
+    start, end = 9_886_058, 9_886_361
+    overlapping_transcripts = transcript_idx.get_overlapping_transcripts(
+        chromosome=chromosome, strand=strand, start=start, end=end
+    )
+    for transcript in overlapping_transcripts:
+        assert transcript.overlaps(chromosome, strand, start, end), (
+            f"Transcript {transcript.transcript_id} does not overlap "
+            f"region {chromosome} {strand} {start} {end}"
+        )
+        assert transcript.gene_name == "Sgk3"
+    assert (
+        len(overlapping_transcripts) == 8
+    ), f"Expected 8 transcripts, found {len(overlapping_transcripts)}"
+    transcript_ids = [x.transcript_id for x in overlapping_transcripts]
+    assert transcript_ids == [
+        "ENSMUST00000166384",
+        "ENSMUST00000188782",
+        "ENSMUST00000168907",
+        "ENSMUST00000188738",
+        "ENSMUST00000188298",
+        "ENSMUST00000171265",
+        "ENSMUST00000097826",
+        "ENSMUST00000191338",
+    ]
+    for i, trans_id in enumerate(transcript_ids):
+        assert transcript_idx.get_transcript(trans_id) == overlapping_transcripts[i]
+
+    # Test that the correct overlapping transcripts are returned
+    start, end = 9_848_250, 9_848_450
+    overlapping_transcripts = transcript_idx.get_overlapping_transcripts(
+        chromosome=chromosome, strand=strand, start=start, end=end
+    )
+    for transcript in overlapping_transcripts:
+        assert transcript.overlaps(chromosome, strand, start, end), (
+            f"Transcript {transcript.transcript_id} does not overlap "
+            f"region {chromosome} {strand} {start} {end}"
+        )
+    assert (
+        len(overlapping_transcripts) == 9
+    ), f"Expected 10 transcripts, found {len(overlapping_transcripts)}"
+    transcript_ids = [x.transcript_id for x in overlapping_transcripts]
+    assert transcript_ids == [
+        "ENSMUST00000166384",
+        "ENSMUST00000188782",
+        "ENSMUST00000168907",
+        "ENSMUST00000188738",
+        "ENSMUST00000188298",
+        "ENSMUST00000171265",
+        "ENSMUST00000188830",
+        "ENSMUST00000189050",
+        "ENSMUST00000097826",
+    ]
+
+    # Try to get a transcript that does not exist
+    assert transcript_idx.get_transcript("ENSMUST00000000000") is None
